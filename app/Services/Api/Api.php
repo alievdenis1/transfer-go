786da580300e2ded58a6de212b7eeda2951132c7
oem <Block2022!>
2022-08-02 17:53:27 +0300

    currencies

---
+++ app/Services/Api/Api.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services\Currencies;
+
+class Currencies
+{
+    const API = 'https://cdn.cur.su/api/latest.json';
+
+    public function list(): array
+    {
+
+        return [];
+    }
+}
