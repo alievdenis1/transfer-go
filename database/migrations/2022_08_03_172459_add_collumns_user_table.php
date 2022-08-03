fc3e1ee50bc5a114d2f74fe922fa8b851d8cbc0e
oem <Block2022!>
2022-08-03 20:34:22 +0300

    new migrate add collumn user

---
+++ database/migrations/2022_08_03_172459_add_collumns_user_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function($table) {
+            $table->integer('min_payment');
+            $table->string('last_name');
+            $table->date('date_birth');
+            $table->date('phone_number');
+            $table->string('city');
+            $table->string('Postcode');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function($table) {
+            $table->dropColumn('min_payment');
+            $table->dropColumn('last_name');
+            $table->dropColumn('date_birth');
+            $table->dropColumn('phone_number');
+            $table->dropColumn('city');
+            $table->dropColumn('Postcode');
+        });
+    }
+};
