fcdb294400064fabda875a367538582e25452e89
denis <denisaliev7@gmail.com>
2022-08-05 23:29:44 +0300

    step1

--- resources/js/components/pages/step1.vue
+++ resources/js/components/pages/step1.vue
@@ -181,16 +181,16 @@
                                     <advantage-banner/>
                                     <choose-deliver/>
                                 </div>
-                                <div class="calculator-content-local" v-if="isLocal">
-                                    <div v-if="!isMoreMinSum" class="calculator-alert alert-warning">
-                                        <svg height="24" viewBox="0 0 24 24" width="24"
-                                             xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M0 0h24v24H0V0z" fill="none"></path>
-                                            <path d="M11 15h2v2h-2zm0-8h2v6h-2zm.99-5C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"></path>
-                                        </svg>
-                                        The amount is too small to transfer
-                                    </div>
+                                <div class="calculator-content calculator-content-local" v-if="isLocal">
                                     <div class="calculator-select-block">
+                                        <div v-if="!isMoreMinSum" class="calculator-alert alert-warning">
+                                            <svg height="24" viewBox="0 0 24 24" width="24"
+                                                 xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M0 0h24v24H0V0z" fill="none"></path>
+                                                <path d="M11 15h2v2h-2zm0-8h2v6h-2zm.99-5C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"></path>
+                                            </svg>
+                                            The amount is too small to transfer
+                                        </div>
                                         <div class="select-block-label">Sending from</div>
                                         <div class="tgc-calculator-select">
 
@@ -368,6 +368,9 @@ export default {
             if (!this.isLocal) {
                 const minSumSendingFrom = this.sendingFrom.currency.equalDollar * this.minSum;
                 return this.sendingFromSum > minSumSendingFrom;
+            } else {
+                const minSumSendingFrom = this.outRegion.currency.equalDollar * this.minSum;
+                return this.outRegionSum > minSumSendingFrom;
             }
 
             return true;
